f918eb89d9d99692548cf88f1f4e5c55829c0bee
Eweaa <yousefmohamed.ym2001@gmail.com>
2023-06-28 19:06:29 +0300

    Progress in Navbar
    - Made Some Progress in the Navbar.
    - Fixed the bookmark icon in HomeCard Component.

--- src/Components/HomeCard/HomeCard.js
+++ src/Components/HomeCard/HomeCard.js
@@ -4,35 +4,52 @@ import { Link } from "react-router-dom";
 
 const HomeCard = (props) => {
 
-    const filledIcon = 
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-fill" viewBox="0 0 16 16">
+    // const filledIcon = 
+    // <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-fill" viewBox="0 0 16 16">
+    //     <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
+    // </svg>;
+
+    // const emptyIcon =
+    // <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark" viewBox="0 0 16 16">
+    //     <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+    // </svg>;
+
+    const Boomkmark = {
+        f:<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-fill" viewBox="0 0 16 16">
         <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
-    </svg>;
-
-    const emptyIcon =
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark" viewBox="0 0 16 16">
+    </svg>,
+        e:<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark" viewBox="0 0 16 16">
         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
-    </svg>;
+    </svg>
+    }
 
-    const [bookmarksIcon, setBookmarkIcon] = React.useState(emptyIcon)
-    const [options, setOptions] = React.useState(0)
+    // const [bookmarksIcon, setBookmarkIcon] = React.useState(emptyIcon)
+    // const [options, setOptions] = React.useState(0)
 
-    const changeIcon = () => {
-        switch (bookmarksIcon) {
-            case emptyIcon:
-                setBookmarkIcon(filledIcon)
-                break;
+    const [EFB, setEFB] = React.useState()
+    let Comp = Boomkmark.e;
+
+    if(EFB === true)
+    Comp = Boomkmark.f
+
+
+    // const changeIcon = () => {
+    //     console.log(Boomkmark)
+    //     switch (bookmarksIcon) {
+    //         case emptyIcon:
+    //             setBookmarkIcon(filledIcon)
+    //             break;
             
-            default:
-                setBookmarkIcon(emptyIcon)
-                break;
-        }
-    }
+    //         default:
+    //             setBookmarkIcon(emptyIcon)
+    //             break;
+    //     }
+    // }
 
-    const showOptions = () => {
-        if (options == 0){setOptions(1)}
-        if (options == 1){setOptions(0)}
-    }
+    // const showOptions = () => {
+    //     if (options == 0){setOptions(1)}
+    //     if (options == 1){setOptions(0)}
+    // }
 
 
     return(
@@ -59,8 +76,8 @@ const HomeCard = (props) => {
             </Link>
 
             <div className={[HomeCardCSS.Tools, 'mt-3'].join(' ')}>
-                <button onClick={changeIcon}>
-                    {bookmarksIcon}
+                <button onClick={() => setEFB(!EFB)}>
+                    {Comp}
                 </button>
             </div>
         </div>
